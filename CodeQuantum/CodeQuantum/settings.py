105c41823b7da5c1f607c8a89281a7e311b90d5e
FraerZip <sensead24@gmail.com>
2024-06-18 18:51:44 +0300

    Setting up an authorization system. Fixing the correct connection to the Java Script file. Minor fixes to the template engine in each HTML document.

--- CodeQuantum/CodeQuantum/settings.py
+++ CodeQuantum/CodeQuantum/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from django.conf.global_settings import AUTH_USER_MODEL
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,13 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static/main/css/main.css',
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
--- CodeQuantum/main/static/main/js/app.js
+++ CodeQuantum/main/static/main/js/app.js
@@ -1,28 +1,25 @@
 $(function() {
-
-    var header = $("#header"),
-        introH = $("#intro").innerHeight(),
-        scrollOffset = $(window).scrollTop();
-
-
     /* Fixed Header */
-    checkScroll(scrollOffset);
-
-    $(window).on("scroll", function() {
-        scrollOffset = $(this).scrollTop();
-
-        checkScroll(scrollOffset);
-    });
-
     function checkScroll(scrollOffset) {
-        if( scrollOffset >= introH ) {
+        var header = $(".header"),
+            introH = $(".intro").innerHeight();
+
+        if (scrollOffset >= introH) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
     }
 
+    if ($(".header").length && $(".intro").length) {
+        var scrollOffset = $(window).scrollTop();
+        checkScroll(scrollOffset);
 
+        $(window).on("scroll", function() {
+            scrollOffset = $(this).scrollTop();
+            checkScroll(scrollOffset);
+        });
+    }
 
     /* Smooth scroll */
     $("[data-scroll]").on("click", function(event) {
@@ -32,36 +29,27 @@ $(function() {
             blockId = $this.data('scroll'),
             blockOffset = $(blockId).offset().top;
 
-        $("#nav a").removeClass("active");
+        $(".nav a").removeClass("active");
         $this.addClass("active");
 
         $("html, body").animate({
-            scrollTop:  blockOffset
+            scrollTop: blockOffset
         }, 500);
     });
 
-
-
     /* Menu nav toggle */
-    $("#nav_toggle").on("click", function(event) {
+    $(".nav-toggle").on("click", function(event) {
         event.preventDefault();
 
         $(this).toggleClass("active");
-        $("#nav").toggleClass("active");
+        $(".nav").toggleClass("active");
     });
 
-
-
-    /* Collapse */
-    $("[data-collapse]").on("click", function(event) {
+    /* Dropdown menu toggle */
+    $(document).on('click', '.nav-item.dropdown .nav__link', function(event) {
         event.preventDefault();
-
-        var $this = $(this),
-            blockId = $this.data('collapse');
-
-        $this.toggleClass("active");
+        $(this).next('.dropdown-menu').toggleClass('show');
     });
-
 
     /* Slider */
     $("[data-slider]").slick({
@@ -70,15 +58,17 @@ $(function() {
         slidesToShow: 1,
         slidesToScroll: 1
     });
-    document.getElementById('price_in_month').addEventListener('input', function (event) {
-        if (event.target.value < 0) {
-            event.target.value = 0;
-        }
-    });
 
-    document.getElementById('full_price').addEventListener('input', function (event) {
-        if (event.target.value < 0) {
-            event.target.value = 0;
+    /* Validation for price input fields */
+    var priceFields = ['price_in_month', 'full_price'];
+    priceFields.forEach(function(fieldId) {
+        var field = document.getElementById(fieldId);
+        if (field) {
+            field.addEventListener('input', function(event) {
+                if (event.target.value < 0) {
+                    event.target.value = 0;
+                }
+            });
         }
     });
 });
